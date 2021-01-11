4a1ac5f135f28e2b03fba5e7d8c46defc15c5790
TuringD <49826893+TuringD@users.noreply.github.com>
2021-01-11 14:51:00 -0600

    Agregar archivos de proyecto.

---
+++ XAM_Crud/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Nancy.Json;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XAM_Crud
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Llenarpropducts();
+
+        }
+
+        private async void Llenarpropducts()
+        {
+            //ejecutar peticion http para conectar con la api rest
+            HttpClient client = new HttpClient();
+            string url = "https://curdproductdhh20210111111325.azurewebsites.net/api/Product";
+            var res = await client.GetAsync(url);
+            var json = res.Content.ReadAsStringAsync().Result;
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            List<Product> products = ser.Deserialize<List<Product>>(json);
+            //Productmodel model = Productmodel.FromJson(json);
+            //listaproducts.ItemsSource = model.Product;
+            string[] Productslst = new string[products.Count];
+            int i = 0;
+            foreach (Product p in products)
+            {
+                Productslst[i] = p.Name;
+                i++;
+            }
+
+            listaproducts.ItemsSource = Productslst;
+        }
+        private async void Agregar_clicl(object sender, EventArgs args)
+        {
+            Product Pd = new Product();
+            Pd.Name = "Mochila1";
+            Pd.Price = 3000;
+            Pd.Creation = DateTime.Now;
+            Pd.Modification = DateTime.Now;
+            HttpClient client = new HttpClient();
+            string url = "https://curdproductdhh20210111111325.azurewebsites.net/api/Product";
+            String JsonP = JsonConvert.SerializeObject(Pd);
+            var res = await client.PostAsync(url, new StringContent(JsonP));
+            var jso = res.Content.ReadAsStringAsync().Result;
+            await DisplayAlert("Resultado", jso, "OK");
+            ;
+        }
+        private async void Elimnar_Click(object sender, EventArgs args)
+        {
+            HttpClient client = new HttpClient();
+            string url = "https://curdproductdhh20210111111325.azurewebsites.net/api/Produc/2";
+            await client.DeleteAsync(url);
+        }
+        private async void Actualizar_Click(object sender, EventArgs args)
+        {
+            Product Pd = new Product();
+            Pd.Id = 3;
+            Pd.Name = "Mochila1";
+            Pd.Price = 3000;
+            Pd.Creation = DateTime.Now;
+            Pd.Modification = DateTime.Now;
+            HttpClient client = new HttpClient();
+            string url = "https://curdproductdhh20210111111325.azurewebsites.net/api/Product";
+            String JsonP = JsonConvert.SerializeObject(Pd);
+            var res = await client.PutAsync( url, new StringContent(JsonP));
+            var jso = res.Content.ReadAsStringAsync().Result;
+            await DisplayAlert("Resultado", jso, "OK");
+        }
+    }
+}
---
+++ XAM_Crud/Productmodel.cs
@@ -0,0 +1,56 @@
+﻿namespace XAM_Crud
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class Productmodel
+    {
+        [JsonProperty("product")]
+        public Product[] Product { get; set; }
+    }
+
+    public partial class Product
+    {
+        [JsonProperty("Id")]
+        public long Id { get; set; }
+
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Price")]
+        public long Price { get; set; }
+
+        [JsonProperty("Creation")]
+        public DateTimeOffset Creation { get; set; }
+
+        [JsonProperty("Modification")]
+        public DateTimeOffset Modification { get; set; }
+    }
+
+    public partial class Productmodel
+    {
+        public static Productmodel FromJson(string json) => JsonConvert.DeserializeObject<Productmodel>(json, XAM_Crud.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Productmodel self) => JsonConvert.SerializeObject(self, XAM_Crud.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
